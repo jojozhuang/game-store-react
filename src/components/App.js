06785736cb8295821dcebfde838d6b5aa06ac4fe
Zhuang, Rong <jojozhuang@gmail.com>
2018-07-31 18:44:30 -0700

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
-import Home from './Home';
-import ProductList from './product/ProductList';
-import ProductPage from './product/ProductPage';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import ProductList from "./product/ProductList";
+import ProductPage from "./product/ProductPage";
 
 const App = () => (
-  <div >
+  <div>
     <Header />
     <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route path="/products" component={ProductList}/>
-      <Route exact path="/productpage" component={ProductPage}/>
-      <Route path="/productpage/:id" component={ProductPage}/>
+      <Route exact path="/" component={ProductList} />
+      <Route path="/products" component={ProductList} />
+      <Route exact path="/productpage" component={ProductPage} />
+      <Route path="/productpage/:id" component={ProductPage} />
     </Switch>
     <Footer />
   </div>
