6591b3ec0147e976803c6980d96be4eb80d3d00f
jojozhuang <jojozhuang@gmail.com>
2021-07-15 07:21:26 -0700

    change year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Footer = () => {  
+const Footer = () => {
   return (
     <div>
       <hr />
       <footer className="container-fluid text-center">
-        <p>&copy; 2017 jojozhuang.github.io, All rights reserved.</p>
+        <p>&copy; 2021 jojozhuang.github.io, All rights reserved.</p>
       </footer>
     </div>
   );
 };
 
-export default Footer; 
+export default Footer;
