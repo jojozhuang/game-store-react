99ae0c6f05654988796fd36b406005c67adc2e02
jojozhuang <jojozhuang@gmail.com>
2025-11-15 23:52:47 -0800

    add basename conditionally

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
+const basename = process.env.REACT_APP_ENV_NAME === "GitHub Pages" ? "/game-store-react" : "/";
+
 ReactDOM.render((
-  <BrowserRouter basename="/game-store-react">
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 ), document.getElementById('root'));
