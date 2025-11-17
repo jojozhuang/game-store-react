ed9887de4eee6a214ee6329216eb04f1141b61da
jojozhuang <jojozhuang@gmail.com>
2025-11-16 19:55:54 -0800

    upgrade webpack merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const merge = require("webpack-merge");
+const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require("webpack");
